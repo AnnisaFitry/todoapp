ce1b3bc5bcce499a50b1de0aa67bf6f39d0e91cb
AnnisaFitry <2041720123@student.polinema.ac.id>
2022-06-12 11:02:50 +0700

    login_register

---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Traits\ApiResponse;
+
+abstract class ApiRequest extends FormRequest
+{
+    use ApiResponse;
+    /**
+     * Get the validation rules that apply to this request.
+     *
+     * @return array
+     */
+    abstract public function rules();
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->apiError( 
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
+    }
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException($this->apiError( 
+            null,
+            Response::HTTP_UNAUTHORIZED,
+        ));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Api\AuthController;
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
